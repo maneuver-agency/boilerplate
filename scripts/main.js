80d346e886b568206a2a44be4bdec01780c516a8
David <david@maneuver.be>
2016-02-09 11:01:58 +0100

    load webfont.js through requirejs

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,11 @@ require.config({
   paths: {
     "googlemaps": "modules/googlemaps",
     "async": "modules/async",
+    "webfont": "http://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js"
   },
 });
 
-require(['polyfills', 'jquery'], function(pol, $){
+require(['polyfills', 'jquery', 'webfont'], function(pol, $){
   require(['components'], function(pol, com){
     require(['utils', 'application'], function(util, app){
       // angular.bootstrap(document, ['instorecomm']);
