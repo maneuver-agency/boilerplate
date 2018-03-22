5fb6a3fcb6ef0b90fa0a53986f90399293f09eab
Yens <yens@maneuver.be>
2018-03-22 09:29:08 +0100

    add missing dependencies in package.json + add @fieldmarshall/deploy

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ Encore
 if (Encore.isProduction()) {
     Encore
         // See postcss.config.js for configuration
-        .enablePostCssLoader()
+        // .enablePostCssLoader()
 
         // create hashed filenames (e.g. app.abc123.css)
         .enableVersioning()
